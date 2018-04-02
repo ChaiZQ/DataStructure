9a323e6de978b0dbbbe4c351500c84caf94abb01
mechaZYK <me_zyk@foxmail.com>
2018-04-02 15:46:22 +0800

    4-2

--- basic_algorithms/test.cpp
+++ basic_algorithms/test.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
-#include <string>
-#include <stack>
-#include <deque>
+#include <vector>
 using namespace std;
-
-
+int solve(int r, int g, int b)
+{
+	if (r == g == b)
+		return r;
+	int a, b, c;
+	if (r > g)
+	{
+		a = r;
+		c = g;
+	}
+}
 int main(int argc, char**argv) {
-
-	system("pause");
+	int n;
+	while (cin >> n) {
+		int r, g, b;
+		cin >> r >> g >> b;
+		cout << solve(r, g, b) << endl;
+	}
 	return 0;
 }
